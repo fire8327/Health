4c37848c1436f5931e6528baf6d99b106ae8a023
fire8327 <fire83274@gmail.com>
2025-07-01 15:16:41 +0300

    create header, footer

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="w-full grid-container bg-[#E6F5EE] mt-auto">
+        <div class="flex flex-col justify-between h-[340px] w-full pt-7 pb-12 font-light">
+            <div class="flex flex-col gap-5">
+                <p class="text-3xl">Контактная информация</p>
+                <div class="flex flex-col leading-[100%]">
+                    <a href="tel:88882052525" class="text-2xl flex flex-col w-fit after:duration-500 after:transition-all after:w-0 after:h-px after:bg-black hover:after:w-full">8 (888) 205-25-25</a>
+                    <a href="mailto:kachestvo@razumed.su" class="text-2xl flex flex-col w-fit after:duration-500 after:transition-all after:w-0 after:h-px after:bg-black hover:after:w-full">kachestvo@razumed.su</a>
+                </div>
+            </div>
+            <div class="flex items-center justify-between text-base">
+                <p>© 2011-2025, Здоровье++. Все права защищены. <br> Не является публичной офертой</p>
+                <p>Учредитель — ООО «Здоровье++».  Адрес для обращений: 350015, <br> Республика Татарстан,  г. Казань, ул. Буденного, д. 182, офис 87.</p>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="w-full grid-container py-4 font-light">
+        <div class="flex flex-col gap-5">
+            <NuxtLink to="/" class="text-3xl">Здоровье++</NuxtLink>
+            <div class="flex items-center justify-between px-16 py-1.5 rounded-full bg-[#E6F5EE] text-center">
+                <NuxtLink to="/">Услуги</NuxtLink>
+                <NuxtLink to="/">Врачи</NuxtLink>
+                <NuxtLink to="/">Запись</NuxtLink>
+                <NuxtLink to="/">О клинике</NuxtLink>
+                <NuxtLink to="/">Контакты</NuxtLink>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
