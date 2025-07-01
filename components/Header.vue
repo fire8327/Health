68d6739213dc2e10501d99a23e3d556185fffd6c
fire8327 <fire83274@gmail.com>
2025-07-01 16:47:04 +0300

    upd header

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,12 @@
 <template>
     <header class="w-full grid-container py-4 font-light">
         <div class="flex flex-col gap-5">
-            <NuxtLink to="/" class="text-3xl">Здоровье++</NuxtLink>
+            <div class="flex items-center justify-between">
+                <NuxtLink to="/" class="text-3xl">Здоровье++</NuxtLink>
+                <NuxtLink to="/" class="text-3xl">
+                    <Icon name="ix:user-profile"/>
+                </NuxtLink>
+            </div>
             <div class="flex items-center justify-between px-16 py-1.5 rounded-full bg-[#E6F5EE] text-center">
                 <NuxtLink to="/services">Услуги</NuxtLink>
                 <NuxtLink to="/doctors">Врачи</NuxtLink>
