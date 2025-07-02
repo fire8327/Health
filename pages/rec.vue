5b0505c841d847aa288f58097773e816863f1079
fire8327 <fire83274@gmail.com>
2025-07-02 18:42:45 +0300

    upd rec

--- pages/rec.vue
+++ pages/rec.vue
@@ -7,12 +7,11 @@
         <FormKit @submit="addRecord" v-if="userId" type="form" :actions="false" messages-class="hidden" form-class="flex flex-col gap-6 items-center justify-center">
             <p class="mainHeading">Записаться</p>
             <div class="flex flex-col gap-4 md:w-2/3 lg:w-1/2">
-                <FormKit validation="required" messages-class="text-[#E9556D]" type="text" placeholder="Специальность" name="Специальность" outer-class="w-full" input-class="w-full px-4 py-1.5 rounded-xl bg-[#F4EFE6]"/>
-                <FormKit v-model="recordForm.doctor_id" validation="required" messages-class="text-[#E9556D]" type="text" placeholder="Врач" name="Врач" outer-class="w-full" input-class="w-full px-4 py-1.5 rounded-xl bg-[#F4EFE6]"/>
+                <FormKit v-model="specialty" validation="required" messages-class="text-[#E9556D]" type="select" :options="specialties" placeholder="Специальность" name="Специальность" outer-class="w-full" input-class="w-full px-4 py-1.5 rounded-xl bg-[#F4EFE6]"/>
+                <FormKit v-if="specialty" v-model="recordForm.doctor_id" validation="required" messages-class="text-[#E9556D]" type="select" :options="doctors" placeholder="Врач" name="Врач" outer-class="w-full" input-class="w-full px-4 py-1.5 rounded-xl bg-[#F4EFE6]"/>
                 <FormKit v-model="recordForm.time" validation="required" messages-class="text-[#E9556D]" type="datetime-local" placeholder="Время" name="Время" outer-class="w-full" input-class="w-full px-4 py-1.5 rounded-xl bg-[#F4EFE6]"/>
             </div>
-            <button type="submit" class="px-10 py-1.5 bg-[#E6F5EE] rounded-full text-center">Создать запись</button>
-            {{ recordForm.time }}
+            <button :disabled="isRecDisabled" :class="{ 'opacity-50 cursor-not-allowed': isRecDisabled }" type="submit" class="cursor-pointer px-10 py-1.5 bg-[#E6F5EE] rounded-full text-center">Создать запись</button>
         </FormKit>
         <p v-else class="text-xl grow flex items-center justify-center">*Для записи необходимо авторизоваться</p>
     </div>
@@ -33,8 +32,46 @@ const recordForm = ref({
 })
 
 
+/* получение специальностей */
+const specialties = ref([])
+const loadSpecialties = async () => {
+    const { data, error } = await supabase
+    .from('specialty')
+    .select('id, name')
+
+    if (error) {
+        console.error(error)
+    }
+
+    specialties.value = data.map(i => ({ label: i.name, value: i.id }))
+}
+
+
+/* получение врачей */
+const specialty = ref(null)
+const doctors = ref([])
+const loadDoctors = async (specialty) => {
+    const { data, error } = await supabase
+    .from('doctors')
+    .select('*')
+    .eq('specialty_id', specialty)
+
+    if (error) {
+        console.error(error)
+    }
+
+    doctors.value = data.map(i => ({ label: i.full_name, value: i.id }))
+}
+
+watch(specialty, () => {
+    loadDoctors(specialty.value)
+})
+
+
 /* добавление записи */
+const isRecDisabled = ref(false)
 const addRecord = async () => {
+    isRecDisabled.value = true
     recordForm.value.time = new Date(recordForm.value.time)
 
     const { data, error } = await supabase
@@ -43,9 +80,16 @@ const addRecord = async () => {
     .select()
 
     if (data) {
+        isRecDisabled.value = false
         showMessage('Запись успешно создана!', true)
     } else {
         showMessage('Произошла ошибка!', false)
     }
 }
+
+
+/* первоначальная загрузка */
+onMounted(() => {
+    loadSpecialties()
+})
 </script>
